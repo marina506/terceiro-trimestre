1fdc63cd10265d781f5f7c59de33163aecb95b35
marina506 <marinaoligomes53@gmail.com>
2025-09-30 11:48:07 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,14 @@
+//arrays -> vetores, matrizes
+// estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["Banana", "Uva", "Laranja"] // array de tamnho 3
+console.log(frutas);
+console.log(frutas[1]);
+
+let numeros = new Array(2,8,9,10);
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "Marina";
+console.log(arrayVazio);
